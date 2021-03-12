c8343321d3240cb2c011b860f920ee09e5223ef6
GRRA611456 <57543146+GRRA611456@users.noreply.github.com>
2021-03-12 11:40:48 +0700

    Update voicecreate.py

--- voicecreate.py
+++ voicecreate.py
@@ -10,12 +10,14 @@ import sqlite3
 import sys
 import os
 
+client = discord.Client()
+
 bot = commands.Bot(command_prefix=".")
 bot.remove_command("help")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN_VOICE_CHANNELS")
 print(DISCORD_TOKEN)
 
-initial_extensions = ['cogs.voice']
+initial_extensions = ['cogs.vc']
 
 if __name__ == '__main__':
     for extension in initial_extensions:
